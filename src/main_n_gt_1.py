51c2f50736885d3ce11c23552334312cb83ba5df
Omer Goldman <Omer.Goldman@nice.com>
2020-04-28 17:59:36 +0300

    automatic labels coverage

--- src/main_n_gt_1.py
+++ src/main_n_gt_1.py
@@ -10,42 +10,18 @@ language = 'ru'
 paradigm = 'N'
 include_only_covered_labels = True
 
-def get_covered_labels():
+def get_covered_labels(data):
     if not include_only_covered_labels:
         return None
-    if language+paradigm == 'esV':
-        covered_labels = {'V;COND;3;SG', 'V;SBJV;PRS;2;SG', 'V.PTCP;PST;MASC;PL', 'V.PTCP;PST;FEM;PL', 'V;IND;PRS;1;PL',
-                          'V;POS;IMP;1;PL', 'V;POS;IMP;3;SG', 'V;IND;PRS;1;SG', 'V;IND;PST;3;SG;IPFV', 'V;IND;PST;3;PL;IPFV',
-                          'V;SBJV;PST;1;SG;LGSPEC1', 'V;IND;PST;3;PL;PFV', 'V;IND;FUT;3;PL', 'V;IND;PST;3;SG;PFV', 'V;NFIN',
-                          'V;IND;FUT;3;SG', 'V;SBJV;PRS;3;PL', 'V;SBJV;PST;3;SG', 'V;COND;3;PL', 'V;POS;IMP;2;SG', 'V;POS;IMP;3;PL',
-                          'V;IND;PST;1;SG;IPFV', 'V;SBJV;PRS;1;SG', 'V;COND;1;SG', 'V;IND;PRS;3;PL', 'V.PTCP;PST;MASC;SG',
-                          'V.PTCP;PST;FEM;SG', 'V.CVB;PRS', 'V;SBJV;PST;3;PL;LGSPEC1', 'V;IND;PRS;2;SG', 'V;SBJV;PST;1;SG',
-                          'V;SBJV;PRS;3;SG', 'V;IND;PRS;3;SG', 'V;SBJV;PST;3;SG;LGSPEC1', 'V;SBJV;PRS;1;PL'}
-    elif language+paradigm == 'fiN':
-        covered_labels = {'N;AT+ESS;SG', 'N;NOM;SG', 'N;PRT;PL', 'N;IN+ABL;SG', 'N;IN+ALL;PL', 'N;AT+ALL;SG', 'N;IN+ABL;PL',
-                          'N;IN+ESS;PL', 'N;ACC;PL', 'N;GEN;SG', 'N;NOM;PL', 'N;AT+ESS;PL', 'N;IN+ALL;SG', 'N;FRML;SG', 'N;ACC;SG',
-                          'N;GEN;PL', 'N;IN+ESS;SG', 'N;PRT;SG'}
-    elif language+paradigm == 'deN' or language+paradigm == 'ruN':
-        covered_labels = None
-    elif language+paradigm == 'frV':
-        covered_labels = {'V;COND;3;SG', 'V;IND;PST;3;PL;IPFV', 'V;IND;PRS;2;PL', 'V;IND;PRS;1;PL', 'V;IND;PST;3;SG;IPFV',
-                          'V;POS;IMP;1;PL', 'V;IND;PRS;3;PL', 'V;NFIN', 'V;POS;IMP;2;PL', 'V;SBJV;PRS;3;PL', 'V;POS;IMP;2;SG',
-                          'V;IND;PST;3;PL;PFV', 'V;IND;PST;3;SG;PFV', 'V;IND;PRS;2;SG', 'V;SBJV;PRS;1;SG', 'V;IND;FUT;3;PL',
-                          'V;IND;FUT;3;SG', 'V.PTCP;PRS', 'V.PTCP;PST', 'V;IND;PRS;1;SG', 'V;IND;PRS;3;SG', 'V;SBJV;PRS;3;SG'}
-    elif language+paradigm == 'ruV':
-        # covered_labels = {'V;PRS;3;PL', 'V;PST;SG;NEUT', 'V;PST;PL', 'V.CVB;PST', 'V;PST;SG;MASC', 'V;PST;SG;FEM',
-        #                   'V;PRS;1;PL', 'V;PRS;3;SG', 'V;PRS;1;SG', 'V.CVB;PRS', 'V;NFIN'}
-        # down is for the separate FUT and PRS case
-        covered_labels = {'V;NFIN', 'V;PRS;3;PL', 'V;PST;PL', 'V;FUT;1;PL', 'V;PRS;1;SG', 'V;IMP;2;SG', 'V;PRS;3;SG',
-                          'V;PST;SG;MASC', 'V;PST;SG;NEUT', 'V;FUT;3;SG', 'V.CVB;PRS', 'V.CVB;PST', 'V;PST;SG;FEM'}
-    elif language+paradigm == 'fiV':
-        covered_labels = {'V;PASS;PST;POS;IND', 'V.PTCP;PASS;PRS', 'V;NFIN', 'V;ACT;PRS;POS;IND;3;PL', 'V.PTCP;ACT;PRS',
-                          'V;ACT;PST;POS;IND;1;SG', 'V;ACT;PST;POS;IND;3;SG', 'V;ACT;PST;POS;IND;3;PL', 'V;ACT;PRS;POS;IND;2;SG',
-                          'V.PTCP;PASS;PST', 'V;ACT;PRS;POS;COND;1;SG', 'V;PASS;PRS;POS;IND', 'V;ACT;PRS;POS;IMP;2;SG',
-                          'V;ACT;PRS;POS;COND;3;PL', 'V;ACT;PRS;POS;COND;3;SG', 'V;PASS;PRS;POS;COND', 'V;ACT;PRS;POS;IND;1;SG',
-                          'V.PTCP;ACT;PST', 'V;ACT;PRS;POS;IND;3;SG'}
-    else:
-        raise NotImplementedError
+    covered_labels = set()
+    for d in data:
+        datum = d[0]
+        datum = datum[datum.index('+')+1:]
+        labels = [datum[:datum.index('+')], datum[datum.index('+') + 1:]]
+        labels = [';'.join(l) for l in labels]
+        assert len(labels)==2
+        covered_labels.add(labels[0])
+        covered_labels.add(labels[1])
     return covered_labels
 
 def pack_params(train_module):
@@ -72,6 +48,8 @@ if __name__ == '__main__':
     write_path = os.path.join(model_dir_path, f'{meta}{addition}_output.txt')
 
     data, added_examples = train_n_gt_1.my_readdata(data_path)
+    covered_labels = get_covered_labels(data)
+    print(f'{covered_labels} covered labels.')
     added_examples_per_label = create_examples_per_label(added_examples)
     train_n_gt_1.init()
     train_n_gt_1.train(train_n_gt_1.model, data)
@@ -87,7 +65,7 @@ if __name__ == '__main__':
     # test_n_gt_1.init()
     # test_n_gt_1.model.populate(model_path)
     all_labels = list(answers[0].keys())
-    accuracy = test_n_gt_1.test(data, None, answers, write_path, added_examples, added_examples_per_label, get_covered_labels())
+    accuracy = test_n_gt_1.test(data, None, answers, write_path, added_examples, added_examples_per_label, covered_labels)
 
 
 
